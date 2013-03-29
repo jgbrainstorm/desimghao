227e342204feb7b236e79cb19e2909889f72287d
jiangang.steven.hao <jiangang.steven.hao@0ff8abb9-d0f9-5276-b013-1b15fb154825>
2013-03-29 19:18:19 +0000

    change directory according to the change

--- getstar.py
+++ getstar.py
@@ -11,7 +11,7 @@ if len(sys.argv) == 1:
     print 'The image need to be reduced'
 else:
    
-   def sex(image, output, sexdir='/usr/remote/user/sispi/jiangang/des-image-hao/imgQualityHao/sexconfig/', check_img=None,config=None):
+   def sex(image, output, sexdir='/usr/remote/user/sispi/jiangang/desimghao/sexconfig/', check_img=None,config=None):
       '''Construct a sextractor command and run it.'''
       if sexdir[-1] != '/':  sexdir += '/'
       com = ["sex", image, "-c "+sexdir+config,"-CATALOG_NAME "+output,"-CHECKIMAGE_NAME "+check_img]
